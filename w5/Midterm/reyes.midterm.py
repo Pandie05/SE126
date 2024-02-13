eeda229c254c3561cc9cec5eb498ee8e308b5655
pandie <hendryreyes35@gmail.com>
2024-02-13 13:47:58 -0500

    end of Midterm

--- w5/Midterm/reyes.midterm.py
+++ w5/Midterm/reyes.midterm.py
@@ -2,23 +2,52 @@
 #Date 2/11/24
 #Midterm
 #Course name: SE126
-#Program Prompt: For your Midterm Project in SE126 you will be building a program of your own design! You must work individually to design a program and file of your choosing.
+#Program Prompt: I asked my friends to give me 2 pokemon, a pokemon city and a pokemon town. I took that info and put it into a csv to then display
+
+#Variable Disctionary
+#snore = Snorelax Count
+#geng = Gengar Count
+#charm = Charmander Count
+#kanto = kanto Region Count
+#lav = Lavender Town Count
+
 #--------------------------------------------------------------------------------------
 
 #import
 import csv
 
-#functions
+#function
+def menu():
 
+    #display options
+    print("\n\n\t-- Menu --")
+    print("1. Show All Data")
+    print("2. Show Pokemon Only")
+    print("3. Show Stats")
+    print("4. Exit")
+
+    #gather option
+    choice = int(input("Enter Your Choice [1 - 4]: "))
+
+    while choice != 1 and choice != 2 and choice != 3 and choice != 4:
+        print("*INVALID ENTRY* DIGITS 1 - 4 ONLY DUMMY")
+
+        choice = int(input("Enter Your Choice [1 - 4]: "))
+
+    #return choice
+    return choice
+
+#make lists
 fname = []
 pokemon1 = []
 pokemon2 = []
 region = []
 city = []
 
-
+#open csv file
 with open("w5/Midterm/pokemon.csv") as csvfile:
 
+    #read and append file
     file = csv.reader(csvfile)
 
     for rec in file:
@@ -29,15 +58,90 @@ with open("w5/Midterm/pokemon.csv") as csvfile:
         region.append(rec[3])
         city.append(rec[4])
 
+#display menu
+answer = menu()
 
-print(f"\n\n{'FIRST':8}  |  {'POKEMON 1':13}  |  {'POKEMON 2':13}  |  {'REGION':7}  |  {'CITY':15}")
-print("------------------------------------------------------------------------------------")
+print('\n--------------------------------------------------\n')
 
-for i in range(0,len(fname)):
+#starting variables
+snore = 0
+geng = 0
+charm = 0
+kanto = 0
+lav = 0
 
-    print(f"{fname[i]:8}  |  {pokemon1[i]:13}  |  {pokemon2[i]:13}  |  {region[i]:7}  |  {city[i]:15}")
+#While Loop start----------------------------------------------------
+while answer != 4:
 
-    print('---------------------------------------------------------------------------------')
+    #display all file data
+    if answer == 1:
+
+        print("\n\t-- Showing all Data --")
+
+        print(f"\n\n{'FIRST':8}  |  {'POKEMON 1':13}  |  {'POKEMON 2':13}  |  {'REGION':7}  |  {'CITY':15}")
+        print("------------------------------------------------------------------------------------")
+
+        for i in range(0,len(fname)):
+
+            print(f"{fname[i]:8}  |  {pokemon1[i]:13}  |  {pokemon2[i]:13}  |  {region[i]:7}  |  {city[i]:15}")
+
+            print('---------------------------------------------------------------------------------')
+
+    #display all pokemon
+    if answer == 2:
+
+        print("\n\t-- Showing Pokemon --")
+
+        for i in range(0,len(fname)):
+
+            print(f"\n\n{pokemon1[i]:13}  |  {pokemon2[i]:13}")
+
+            print('--------------------------------------------')
+
+    #gather stats
+    if answer == 3:
+
+        print("\t-- Showing Stats --")
+
+        for i in range(0, len(fname)):
+
+            if pokemon1[i] == "Snorlax" or pokemon2[i] == "Snorlax":
+
+                snore += 1
 
 
+            if pokemon1[i] == "Gengar" or pokemon2[i] == "Gengar":
 
+                geng += 1
+
+
+            if pokemon1[i] == "Charmander" or pokemon2[i] == "Charmander":
+
+                charm += 1
+
+
+            if region[i] == "Kanto":
+
+                kanto += 1
+
+
+            if city[i] == "Lavender Town":
+
+                lav += 1
+
+        #display stats
+        print(f"\n\nSnorelax was chosen {snore} times!")
+        print(f"Gengar was chosen {geng} times!")
+        print(f"Charmander was chosen {charm} times!")
+        print(f"Kanto was the most popular region and was chosen {kanto} times!")
+        print(f"Lavender Town was the most popular city in the Kanto region chosen {lav} times!")
+
+        print('\n-------------------------------------------------------------------------------------------')
+
+    #show menu again 
+    answer = menu()
+
+#While Loop end------------------------------------------------------------------------------------------
+        
+#goodbye 
+print("\n\nGoodbye :)")
