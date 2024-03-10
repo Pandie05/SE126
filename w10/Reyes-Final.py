0f9e1bb154b67a8ff7b0c1118c16235ec130f432
pandie <hendryreyes35@gmail.com>
2024-03-10 00:03:51 -0500

    caca

---
+++ w10/Reyes-Final.py
@@ -0,0 +1,25 @@
+#Name: Hendry
+#Date 3/9/24
+#Final (woooooooooooo)
+#Course name: SE126
+#Prompt:
+
+
+#----------------------------------------------------------------------
+
+#hello :)
+
+
+#---Functions-------------------------------------
+
+#---Main Code-------------------------------------
+
+#make list---
+
+#inputs---
+
+#choose starter---
+
+#choose legendary---
+
+#---Seq Search--------------------------------------
