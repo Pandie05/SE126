12bb2a330d5165ad542b150041dacb7ae225eae1
pandie <hendryreyes35@gmail.com>
2024-03-12 22:27:31 -0400

    mid final

--- w10/Reyes-Final.py
+++ w10/Reyes-Final.py
@@ -6,6 +6,8 @@
 
 
 #----------------------------------------------------------------------
+from os import system, name 
+from time import sleep 
 
 #hello :)
 print("\nWelcome to the Pokemon Character Creator :)")
@@ -29,12 +31,14 @@ def menu():
     
     return menu_choice
 
+#--------------------------------------------------------------------------------------------------------------
+ 
+
 #---Main Code-------------------------------------
 
 #make list---
 name = []
 starter = []
-legend = []
 region = []
 
 generation = ['Gen 1', 'Gen 2', 'Gen 3', 'Gen 4','Gen 5', 'Gen 6', 'Gen 7', 'Gen 8', 'Gen 9']
@@ -42,103 +46,310 @@ grass = ['Bulbasaur', 'Chikorita', 'Treecko', 'Turtwig', 'Snivy', 'Chespin', 'Ro
 water = ['Squirtle', 'Totodile', 'Mudkip', 'Piplup', 'Oshawott', 'Froakie', 'Popplio', 'Sobble', 'Quaxly']
 fire = ['Charmander', 'Cyndaquil', 'Torchic', 'Chimchar','Tepig', 'Fennekin', 'Litten', 'Scorbunny', 'Fuecoco']
 
-for i in range(0, len(generation)):
-
-    print(f"{generation[i]:10}: {grass[i]:12} {water[i]:12} {fire[i]}")
-
 choice = menu()
 
-while choice != '5':
+while choice != '4':
 
+    #--------------------- Choice 1 --------------------------
     if choice == '1':
 
         print('showing all characters made...')
 
         for i in range(len(name)):
 
-            print(f"{name[i]} {starter[i]} {legend[i]} {region[i]}")
+            print(f"{name[i]:10} {starter[i]:10} {region[i]:10}") 
+        
+        
 
-
-
+    #--------------------- Choice 2 --------------------------
     if choice == '2':
 
-        print('Choosing name...')
+        print('\n\n\tChoosing name...')
 
         char_name = input("Please Enter Character Name: ")
 
         name.append(char_name)
 
-        print(name)
+        print("\n\n\tchoosing starter...")
 
-        print("choosing starter...")
+        for i in range(0, len(generation)):
 
-        gen = input("Please Choose Generation [1-9]: ")
+            print(f"{generation[i]}{':':5} {grass[i]:12} {water[i]:12} {fire[i]}")
 
+        gen = input("\nPlease Choose Generation [1-9]: ")
+
+
+        #----------------------- Gen 1 ------------------------------------------------------
         if gen == '1':
 
-            print("Gen 1: Bulbasaur, Squirtle, Charmander, Pikachu")
+            print(f"\n{fire[0]:10}{'|'}{water[0]:10}{'|'}{fire[0]:10}")
 
-            gen_choice = input("Please Choose Pokemone [1-4] ")
+            gen_choice = input("\nPlease Choose Pokemone [1-3]: ")
 
             if gen_choice == '1':
 
-                pokemon = "Bulbasaur"
-                starter.append(pokemon)
+                starter.append(grass[0])
 
             if gen_choice == '2':
                 
-                pokemon = "Squirtle"
-                starter.append(pokemon)
+                starter.append(water[0])
 
             if gen_choice == '3':
                 
-                pokemon = "Charmander"
-                starter.append(pokemon)
+                starter.append(fire[0])
 
-            if gen_choice == '4':
-                
-                pokemon = "Pikachu"
-                starter.append(pokemon)
-
+        #----------------------- Gen 2 ------------------------------------------------------
         if gen == '2':
-            print('')
 
+            print(f"\n{fire[1]:10}{'|'}{water[1]:10}{'|'}{fire[1]:10}")
+
+            gen_choice = input("\nPlease Choose Pokemone [1-3] ")
+
+            if gen_choice == '1':
+
+                starter.append(grass[1])
+
+            if gen_choice == '2':
+                
+                starter.append(water[1])
+
+            if gen_choice == '3':
+                
+                starter.append(fire[1])
+
+        #----------------------- Gen 3 ------------------------------------------------------
         if gen == '3':
-            print('')
 
+            print(f"\n{fire[2]:10}{'|'}{water[2]:10}{'|'}{fire[2]:10}")
+
+            gen_choice = input("\nPlease Choose Pokemone [1-3] ")
+
+            if gen_choice == '1':
+
+                starter.append(grass[2])
+
+            if gen_choice == '2':
+                
+                starter.append(water[2])
+
+            if gen_choice == '3':
+                
+                starter.append(fire[2])
+
+        #----------------------- Gen 4 ------------------------------------------------------
         if gen == '4':
-            print('')
 
+            print(f"\n{fire[3]:10}{'|'}{water[3]:10}{'|'}{fire[3]:10}")
+
+            gen_choice = input("\nPlease Choose Pokemone [1-3] ")
+
+            if gen_choice == '1':
+
+                starter.append(grass[3])
+
+            if gen_choice == '2':
+                
+                starter.append(water[3])
+
+            if gen_choice == '3':
+                
+                starter.append(fire[3])
+
+        #----------------------- Gen 5 ------------------------------------------------------
         if gen == '5':
-            print('')
 
+            print(f"\n{fire[4]:10}{'|'}{water[4]:10}{'|'}{fire[4]:10}")
+
+            gen_choice = input("\nPlease Choose Pokemone [1-3] ")
+
+            if gen_choice == '1':
+
+                starter.append(grass[4])
+
+            if gen_choice == '2':
+                
+                starter.append(water[4])
+
+            if gen_choice == '3':
+                
+                starter.append(fire[4])
+
+        #----------------------- Gen 6 ------------------------------------------------------
         if gen == '6':
-            print('')
 
+            print(f"\n{fire[5]:10}{'|'}{water[5]:10}{'|'}{fire[5]:10}")
+
+            gen_choice = input("\nPlease Choose Pokemone [1-3] ")
+
+            if gen_choice == '1':
+
+                starter.append(grass[5])
+
+            if gen_choice == '2':
+                
+                starter.append(water[5])
+
+            if gen_choice == '3':
+                
+                starter.append(fire[5])
+
+        #----------------------- Gen 7 ------------------------------------------------------
         if gen == '7':
-            print('')
 
+            print(f"\n{fire[6]:10}{'|'}{water[6]:10}{'|'}{fire[6]:10}")
+
+            gen_choice = input("\nPlease Choose Pokemone [1-3] ")
+
+            if gen_choice == '1':
+
+                starter.append(grass[6])
+
+            if gen_choice == '2':
+                
+                starter.append(water[6])
+
+            if gen_choice == '3':
+                
+                starter.append(fire[6])
+
+        #----------------------- Gen 8 ------------------------------------------------------
         if gen == '8':
-            print('')
 
+            print(f"\n{fire[7]:10}{'|'}{water[7]:10}{'|'}{fire[7]:10}")
+
+            gen_choice = input("\nPlease Choose Pokemone [1-3] ")
+
+            if gen_choice == '1':
+
+                starter.append(grass[7])
+
+            if gen_choice == '2':
+                
+                starter.append(water[7])
+
+            if gen_choice == '3':
+                
+                starter.append(fire[7])
+
+        #----------------------- Gen 9 ------------------------------------------------------
         if gen == '9':
-            print('')
+
+            print(f"\n{fire[8]:10}{'|'}{water[8]:10}{'|'}{fire[8]:10}")
+
+            gen_choice = input("\nPlease Choose Pokemone [1-3] ")
+
+            if gen_choice == '1':
+
+                starter.append(grass[8])
+
+            if gen_choice == '2':
+                
+                starter.append(water[8])
+
+            if gen_choice == '3':
+                
+                starter.append(fire[8])
+
+        #------------------ Region Choice -------------------------------------
+
+        print("\n\n\tChoose region")
+
+        print("\nRegion 1: Kanto \n Region 2: Johto")
+        print("\nRegion 3: Hoenn \n Region 4: Sinnoh")
+        print("\nRegion 5: Unova \n Region 6: Kalos")
+        print("\nRegion 7: Alola \n Region 8: Galar")
+        print("\nRegion 9: Paldea")
+
+        region_choice = input("\nChoose Region [1-9]: ")
+
+        if region_choice == '1':
+
+            region.append(region_choice)
 
 
+        if region_choice == '2':
 
-        choice = menu()
+            region.append(region_choice)
+
+
+        if region_choice == '3':
+
+            region.append(region_choice)
+
+
+        if region_choice == '4':
+
+            region.append(region_choice)
+
+
+        if region_choice == '5':
+
+            region.append(region_choice)
+
+
+        if region_choice == '6':
+
+            region.append(region_choice)
+
+
+        if region_choice == '7':
+
+            region.append(region_choice)
+
+
+        if region_choice == '8':
+
+            region.append(region_choice)
+
+
+        if region_choice == '9':
+
+            region.append(region_choice)
+
+
+    #--------------------- Choice 3 --------------------------
 
     if choice == '3':
 
-        print('choosing pokemon...')
+        print('\n\n\tsearching...')
+
+        search_name = input("\nPlease Enter name you are looking for: ")
+
+        found = []
+
+        for i in range(0, len(name)):
+
+            if search_name.lower() == name[i].lower():
+
+                found.append(i)
+
+        if found[0] != "":
+
+            print(f"\n\tWe found {search_name} at index {found} ")
+            print("\tHere is their info: \n")
+
+            print(f"\t\t{'NAME':10} {'STARTER':10} {'REGION':10}")
+            print("\t\t-------------------------------")
+
+            for i in range(0, len(found)):
+
+                print(f"\t\t{name[found[i]]:10} {starter[found[i]]:10} {region[found[i]]:10}")
+
+        else:
+            print(f"\n\tWe could not find {search_name} ")
+
+    choice = menu()
+
+#-------------- Seq Search --------------------------
 
 
 
-    if choice == '4':
-        print('')
-     
 
 
+#--------------------------------------------------------
+print('\n\n\tshowing all characters made...')
 
-    
-#---Seq Search--------------------------------------
+for i in range(len(name)):
+
+    print(f"{name[i]:10} {starter[i]:10} {region[i]:10}")
+
